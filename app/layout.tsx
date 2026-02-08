50dd249b615008856572229383e3dd05e4135edd
Lazar Šćekić <67110599+lazarpmf@users.noreply.github.com>
2026-02-08 13:36:20 +0100

    meta-tag update

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,6 +77,10 @@ export default function RootLayout({
     <html lang="sr">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes" />
+        <meta
+          name="google-site-verification"
+          content="tZT3HRecKIBbZTpbskEBABm32KtNYXEnGODcSmWV1-I"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
